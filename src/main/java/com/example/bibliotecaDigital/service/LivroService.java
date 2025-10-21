6c67d36e1611c5fed9720ebc9a73d3c20a45f91b
SeijiHoshino1 <seijihoshino@outlook.com>
2025-10-21 01:33:38 -0300

    feat(service): add service

---
+++ src/main/java/com/example/bibliotecaDigital/service/LivroService.java
@@ -0,0 +1,51 @@
+package com.example.bibliotecaDigital.service;
+
+import com.example.bibliotecaDigital.model.Livro;
+import com.example.bibliotecaDigital.repository.JpaLivroRepository;
+import com.example.bibliotecaDigital.view.LivroView;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class LivroService extends LivroView {
+
+    private final JpaLivroRepository jpaLivroRepository;
+
+    public List<Livro> findAll() {
+        return jpaLivroRepository.findAll();
+    }
+
+    public Optional<Livro> findById(UUID uuid) {
+        return jpaLivroRepository.findById(uuid);
+    }
+
+    @Transactional
+    public Livro save(Livro livro) {
+        return jpaLivroRepository.save(livro);
+    }
+
+    @Transactional
+    public Livro update(UUID uuid, Livro livro) {
+        jpaLivroRepository.findById(uuid);
+
+        livro.setTitulo(livro.getTitulo());
+        livro.setAutor(livro.getAutor());
+        livro.setCategoria(livro.getCategoria());
+        livro.setQtd_paginas(livro.getQtd_paginas());
+
+        jpaLivroRepository.save(livro);
+
+        return livro;
+    }
+
+    @Transactional
+    public void delete(Livro livro) {
+        jpaLivroRepository.delete(livro);
+    }
+}
