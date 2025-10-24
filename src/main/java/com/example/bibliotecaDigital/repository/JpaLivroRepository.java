0e69c3d2bb3045d2c3ad50e5c4a6e0ca4e1ca9c4
SeijiHoshino1 <seijihoshino@outlook.com>
2025-10-24 01:29:58 -0300

    chore(repository): change id type to Long

--- src/main/java/com/example/bibliotecaDigital/repository/JpaLivroRepository.java
+++ src/main/java/com/example/bibliotecaDigital/repository/JpaLivroRepository.java
@@ -7,5 +7,5 @@ import org.springframework.stereotype.Repository;
 import java.util.UUID;
 
 @Repository
-public interface JpaLivroRepository extends JpaRepository<Livro, UUID> {
+public interface JpaLivroRepository extends JpaRepository<Livro, Long> {
 }
